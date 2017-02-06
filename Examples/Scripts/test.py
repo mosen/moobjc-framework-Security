7c800c0f64570461775f41566301a9e619ede6e1
Mosen <mosen1@gmail.com>
2017-02-06 14:33:09 +1100

    More signature fixing for functions with out pointer parameters.

--- Examples/Scripts/test.py
+++ Examples/Scripts/test.py
@@ -1,8 +1,21 @@
 import Security
 
 status, kc = Security.SecKeychainOpen('~/Library/Keychains/login.keychain', None)
-if status == 0:
-    print('success')
+if status != 0:
+    raise Exception('Couldnt open keychain')
+
+status, settings = Security.SecKeychainCopySettings(kc, None)
+if status != 0:
+    raise Exception('Couldnt get settings')
+
+print(settings)
+
+status, searchList = Security.SecKeychainCopyDomainSearchList(Security.kSecPreferencesDomainUser, None)
+if status != 0:
+    raise Exception('Couldnt get user domain search list')
+
+print(searchList)
+
 
 
 
